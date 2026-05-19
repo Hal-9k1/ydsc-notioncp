bc94074b057bbe0f0241337fd5db6c42e4232080
Eddy Meals <edimea211@gmail.com>
2026-05-18 23:42:29 -0700

    Put the cart way before the horse and set up pyinstaller

--- notioncp.py
+++ notioncp.py
@@ -1,5 +1,6 @@
 import argparse
 import notion_client as nc
+import sys
 from datetime import datetime
 try:
   import env
@@ -9,7 +10,7 @@ except ImportError:
 def main(arglist):
   parser = argparse.ArgumentParser(
     prog='notioncp',
-    desc=(
+    description=(
       'Internal utility for Yonder Dynamics Science Lab subteam to '
       'upload test result CSVs to Notion.'
     ),
@@ -18,13 +19,13 @@ def main(arglist):
     '--token-file',
     required=env is None,
     help=(
-      f'{"Required because %{prog}s was packaged without an API key. " if env is None else ""}'
+      f'{"Required because %(prog)s was packaged without an API key. " if env is None else ""}'
       'The path to a file containing the Notion API key.'
     )
   )
   parser.add_argument(
     '--date',
-    metavar='MM-DD-YYYY'
+    metavar='MM-DD-YYYY',
     type=lambda s: datetime.strptime(s, '%m-%d-%Y'),
     help=(
       'If given, overrides the timestamp the uploaded CSVs are marked with. '
